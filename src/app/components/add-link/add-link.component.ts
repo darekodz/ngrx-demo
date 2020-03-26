d32cd1bc8ff019cdb0ad8b0bc5738d8ba9f8b1a1
Dariusz Odziomek <dodziomek@smartadserver.com>
2020-03-26 15:36:44 +0100

    improved form

--- src/app/components/add-link/add-link.component.ts
+++ src/app/components/add-link/add-link.component.ts
@@ -1,31 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { add } from '../../store/links.actions';
-import { FormBuilder } from '@angular/forms';
-import { Link } from 'src/app/store/links.reducer';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-link',
   templateUrl: './add-link.component.html',
   styleUrls: ['./add-link.component.scss']
 })
-export class AddLinkComponent {
-  checkoutForm;
+export class AddLinkComponent implements OnInit {
+  checkoutForm: FormGroup;
 
-  emptyForm: Link = {
-    name: '',
-    url: '',
-    description: ''
-  };
+  constructor(private store: Store) {}
 
-  constructor(private store: Store, private formBuilder: FormBuilder) {
-    this.checkoutForm = this.formBuilder.group(this.emptyForm);
+  ngOnInit(): void {
+    const reg = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
+
+    this.checkoutForm = new FormGroup({
+      name: new FormControl(null, [
+        Validators.required
+      ]),
+      url: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(reg)
+      ]),
+      description: new FormControl(null, [
+        Validators.required
+      ]),
+    });
   }
 
-  onSubmit(data: Link) {
-    this.checkoutForm.reset();
-
-    this.store.dispatch(add(data));
+  onSubmit(form: FormGroup) {
+    if (form.valid) {
+      this.store.dispatch(add(form.value));
+      this.checkoutForm.reset();
+    } else {
+      console.warn('form invalid');
+    }
   }
 
 }

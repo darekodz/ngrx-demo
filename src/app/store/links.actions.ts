1e6e1e73bd93a593813d89462ef2edb911f157b0
Dariusz Odziomek <dodziomek@smartadserver.com>
2020-03-27 22:49:10 +0100

    remove all

--- src/app/store/links.actions.ts
+++
@@ -1,9 +0,0 @@
-import { createAction, props, State } from '@ngrx/store';
-import { Link } from './links.reducer';
-
-export const add = createAction(
-    'Add',
-    props<Link>()
-);
-
-export const reset = createAction('Reset');
